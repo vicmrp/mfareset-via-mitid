86aeff27c0ba7a5c95dab23ae1497d53728b4231
Victor Reipur <victor@reipur.dk>
2026-03-31 17:30:25 +0200

    chore: renamed function to require can_delete to be true, before user can delete the mfa-method

--- core/utils/auth_methods.py
+++ core/utils/auth_methods.py
@@ -1,10 +1,11 @@
-def prettify_auth_methods(methods):
+def prepare_auth_methods(methods):
     pretty = []
 
     for method in methods:
         odata_type = method.get("@odata.type", "")
         label = "Unknown method"
         details = ""
+        can_delete = False
 
         if odata_type.endswith("passwordAuthenticationMethod"):
             label = "Password"
@@ -14,18 +15,22 @@ def prettify_auth_methods(methods):
 
         elif odata_type.endswith("phoneAuthenticationMethod"):
             label = "Phone"
+            can_delete = True
             phone_type = method.get("phoneType", "")
             phone_number = method.get("phoneNumber", "")
             details = f"{phone_type}: {phone_number}"
 
         elif odata_type.endswith("microsoftAuthenticatorAuthenticationMethod"):
             label = "Microsoft Authenticator"
+            can_delete = True
             display_name = method.get("displayName", "")
             device_tag = method.get("deviceTag", "")
             details = f"{display_name} ({device_tag})"
 
+
         elif odata_type.endswith("softwareOathAuthenticationMethod"):
             label = "Software OATH token"
+            can_delete = True
 
         elif odata_type.endswith("windowsHelloForBusinessAuthenticationMethod"):
             label = "Windows Hello for Business"
@@ -42,8 +47,8 @@ def prettify_auth_methods(methods):
                 "details": details,
                 "id": method.get("id", ""),
                 "type": odata_type,
+                "can_delete": can_delete,
             }
         )
 
     return pretty
-
--- core/views.py
+++ core/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from core.utils.graph import list_user_authentication_methods
-from core.utils.auth_methods import prettify_auth_methods
+from core.utils.auth_methods import prepare_auth_methods
 import logging
 
 logger = logging.getLogger(__name__)
@@ -23,7 +23,7 @@ def profile(request):
             username = f"{username}@dtu.dk"
 
         raw_methods = list_user_authentication_methods(username)
-        auth_methods = prettify_auth_methods(raw_methods)
+        auth_methods = prepare_auth_methods(raw_methods)
     except Exception:
         logger.exception("Failed to fetch authentication methods for %s", username)
         graph_error = "Could not retrieve authentication methods at the moment."
